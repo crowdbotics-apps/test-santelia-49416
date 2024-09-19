b52244e8d1c640fb384c4fce00148b0ad5c8cd5c
Crowdbotics <team@crowdbotics.com>
2024-09-19 18:57:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-santelia-49416",
+  url: "https://test-santelia-49416.botics.co",
+  api: "https://test-santelia-49416.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
